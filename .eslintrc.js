320503506d254cc84998bdc54e851ff2e607f68a
Tam Le <tamsgit@gmail.com>
2023-06-12 12:41:41 -0400

    lint: Install & configure eslint
    - Initilize eslint config via command:
    ```
    npm init @eslint/config
    ```
    - Reference for React config: https://github.com/jsx-eslint/eslint-plugin-react#configuration-legacy-eslintrc-

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "react/prop-types": "off",
+    },
+};
